5a0b63ec6fcf6a90e6f00414364d7c7f32009d41
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2025-02-21 13:29:41 -0500

    Simple config for libcurl

--- Makefile
+++ Makefile
@@ -1,12 +1,8 @@
-# turn object file into merge executable (linking stage)
-# -g for debugging info
 bacon: main.o
-	g++ -o bacon main.o
+	g++ -o bacon main.o -lcurl
 
-# create object file from source (compilation stage)
 main.o: main.cpp
 	g++ -c main.cpp
 
-# remove object file and executable
 clean:
 	rm *.o bacon
--- main.cpp
+++ main.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
+#include <curl/curl.h>
+#include <stdlib.h>
+#include <string.h>
+
+size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata);
+
+struct memory {
+    char *response;
+    size_t size;
+};  
 
 int main(int argc, char** argv) {
+    CURL *curl = curl_easy_init();
+    if (!curl) {
+        std::cerr << "Failed to make handle" << std::endl;
+        exit(1);
+    }
+    CURLcode response;
+    
+    curl_easy_setopt(curl, CURLOPT_URL, "https://example.com/");
+    std::string out;
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *) &out);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+    response = curl_easy_perform(curl);
+    if (response) {
+        std::cerr << curl_easy_strerror(response) << std::endl;
+        exit(1);
+    }
+    std::cout << out << std::endl;
+    curl_easy_cleanup(curl);
     return 0;
+}
+
+size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
+    std::string* str = (std::string*)userdata;
+    for (size_t i = 0; i < nmemb; ++i) {
+        str->push_back(ptr[i]);
+    }
+    return nmemb;
 }
