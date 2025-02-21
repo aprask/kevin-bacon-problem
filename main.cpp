95d986d7444823f7a49584b4e7f763e385e49a1f
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2025-02-21 18:04:10 -0500

    Stopping point for today -- was able to parse json

--- main.cpp
+++ main.cpp
@@ -9,27 +9,44 @@
 #define ARGS 4
 
 size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata);
-std::string req_hand(const std::string& firstName, const std::string& lastName, std::string* url);
+std::string req_hand(const std::string& name, std::string url);
 void parse_json(const std::string& res_bod, std::vector<std::string>* res_buffer);
+void traverseGraph(const std::string& name, const size_t& depth, std::vector<std::string> urls);
 
 int main(int argc, char** argv) {
     if (argc != ARGS) {
         std::cerr << "Entered <" << argc << ">. Expected <" << ARGS << ">" << std::endl;
         return 1;
     }
-
-    std::string firstName = argv[1];
-    std::string lastName = argv[2];
-    size_t depth = std::stol(argv[3]);
-
+    std::string param = "%20";
+    const std::string name = argv[1] + param + argv[2];
+    const size_t depth = std::stol(argv[3]);
     std::string url = "http://hollywood-graph-crawler.bridgesuncc.org/neighbors/";
-    std::string out = req_hand(firstName, lastName, &url);
-    std::vector<std::string> res_buffer;
-    parse_json(out, &res_buffer);
-    for (int i = 0; i < res_buffer.size(); ++i) {
-        std::cout << res_buffer[i] << std::endl;
-    }
+    std::vector<std::string> urls;
+    urls.push_back(url);
+    traverseGraph(name, depth, urls);
     return 0;
+}
+
+void traverseGraph(const std::string& name, const size_t& depth, std::vector<std::string> urls) {
+    int i = 0;
+    std::vector<std::string> res_buffer;
+    std::string out;
+    int k;
+    int j;
+    urls[0] = urls[0] + name;
+    while (i < depth) {
+        for (k = 0; k < urls.size(); ++k) {
+            out = req_hand(name, urls[k]);
+            parse_json(out, &res_buffer);
+            for (j = 0; j < res_buffer.size(); ++j) {
+                std::cout << res_buffer[j] << std::endl;
+            }
+            std::cout << std::endl;
+        }
+        res_buffer.clear();
+        i++;
+    }
 }
 
 void parse_json(const std::string& res_bod, std::vector<std::string>* res_buffer) {
@@ -60,15 +77,14 @@ size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
     return nmemb;
 }
 
-std::string req_hand(const std::string& firstName, const std::string& lastName, std::string* url) {
+std::string req_hand(const std::string& name, std::string url) {
     CURL *curl = curl_easy_init();
     if (!curl) {
         std::cerr << "Failed to make handle" << std::endl;
         exit(1);
     }
     CURLcode response;
-    *url = *url + firstName + "%20" + lastName;
-    curl_easy_setopt(curl, CURLOPT_URL, url->c_str());
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
     std::string out;
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *) &out);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
@@ -80,3 +96,14 @@ std::string req_hand(const std::string& firstName, const std::string& lastName, 
     curl_easy_cleanup(curl);
     return out;
 }
+
+
+/*
+Structure
+l1: HEAD
+l2: HEAD's Movies
+l3: Movie's actors
+l4: Actor's movies
+l5: Movie's actors
+l6: Actor's movies
+*/
